97b3d27bde7e8a0e87489f0acfaee58e0b6a6dad
richard-hartnell <richard@richardhartnell.com>
2023-09-19 20:23:00 -0700

    Added 'look inside' and 'reviews' section

--- script.js
+++ script.js
@@ -2,39 +2,58 @@ const menu = document.getElementById('frontpage');
 const glimpse = document.getElementById('glimpse-wrapper');
 const crew = document.getElementById('crew-wrapper');
 const calendar = document.getElementById('calendar-wrapper');
+const reviews = document.getElementById('reviews-wrapper');
 
 const menu1 = document.getElementById('menu1');
 const menu2 = document.getElementById('menu2');
 const menu3 = document.getElementById('menu3');
 const menu4 = document.getElementById('menu4');
+const menu5 = document.getElementById('menu5');
 
 function showMenu(menuId) {
 
     if (menuId === 'glimpse') {
       menu.style.display = 'none';
       glimpse.style.display = 'flex';
-      crew.style.display = 'none';
-      calendar.style.display = 'none';
+      // crew.style.display = 'none';
+      // calendar.style.display = 'none'
+      reviews.style.display = 'none';
       menu1.style.fontWeight = 'normal';
       menu2.style.fontWeight = 'bold';
-      menu3.style.fontWeight = 'normal';
-      menu4.style.fontWeight = 'normal'}
+      // menu3.style.fontWeight = 'normal';
+      // menu4.style.fontWeight = 'normal';
+      menu5.style.fontWeight = 'normal'}
     else if (menuId === 'crew') {
       menu.style.display = 'none';
       glimpse.style.display = 'none';
-      crew.style.display = 'block';
-      calendar.style.display = 'none';
+      // crew.style.display = 'block';
+      // calendar.style.display = 'none'
+      reviews.style.display = 'none';
       menu1.style.fontWeight = 'normal';
       menu2.style.fontWeight = 'normal';
-      menu3.style.fontWeight = 'bold';
-      menu4.style.fontWeight = 'normal';
+      // menu3.style.fontWeight = 'bold';
+      // menu4.style.fontWeight = 'normal'
+      menu5.style.fontWeight = 'normal';
     } else if (menuId === 'calendar') {
       menu.style.display = 'none';
       glimpse.style.display = 'none';
-      crew.style.display = 'none';
-      calendar.style.display = 'block';
+      // crew.style.display = 'none';
+      // calendar.style.display = 'block'
+      reviews.style.display = 'none';
       menu1.style.fontWeight = 'normal';
       menu2.style.fontWeight = 'normal';
-      menu3.style.fontWeight = 'normal';
-      menu4.style.fontWeight = 'bold';
-}}
+      // menu3.style.fontWeight = 'normal';
+      // menu4.style.fontWeight = 'bold'
+      menu5.style.fontWeight = 'normal';
+    } else if (menuId === 'reviews') {
+      menu.style.display = 'none';
+      glimpse.style.display = 'none';
+      // crew.style.display = 'none';
+      // calendar.style.display = 'none'
+      reviews.style.display = 'flex';
+      menu1.style.fontWeight = 'normal';
+      menu2.style.fontWeight = 'normal';
+      // menu3.style.fontWeight = 'normal';
+      // menu4.style.fontWeight = 'normal'
+      menu5.style.fontWeight = 'bold';
+    }}
