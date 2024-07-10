104b767f08646d529ced0e7a3aaa39b8cecc44e7
richard-hartnell <richard@richardhartnell.com>
2024-07-09 22:02:27 -0700

    Fixed the section gore. Still needs new CSS

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const glimpse = document.getElementById('glimpse-wrapper');
 const crew = document.getElementById('crew-wrapper');
 const music = document.getElementById('music-wrapper');
 const reviews = document.getElementById('reviews-wrapper');
-
+   
 const menu1 = document.getElementById('menu1');
 const menu2 = document.getElementById('menu2');
 const menu3 = document.getElementById('menu3');
@@ -21,52 +21,53 @@ function toggleBurger() {
 }
 
 function showMenu(menuId) {
-  burgerMenu.style.display = 'none';
+  toggleBurger()
 
-    if (menuId === 'glimpse') {
-      menu.style.display = 'none';
-      glimpse.style.display = 'flex';
-      crew.style.display = 'none';
-      music.style.display = 'none';
-      reviews.style.display = 'none';
-      menu1.style.fontWeight = 'normal';
-      menu2.style.fontWeight = 'bold';
-      menu3.style.fontWeight = 'normal';
-      menu4.style.fontWeight = 'normal';
-      menu5.style.fontWeight = 'normal'}
-    else if (menuId === 'crew') {
-      menu.style.display = 'none';
-      glimpse.style.display = 'none';
-      crew.style.display = 'block';
-      music.style.display = 'none';
-      reviews.style.display = 'none';
-      menu1.style.fontWeight = 'normal';
-      menu2.style.fontWeight = 'normal';
-      menu3.style.fontWeight = 'bold';
-      menu4.style.fontWeight = 'normal';
-      menu5.style.fontWeight = 'normal';
-    } else if (menuId === 'music') {
-      menu.style.display = 'none';
-      glimpse.style.display = 'none';
-      crew.style.display = 'none';
-      music.style.display = 'block';
-      reviews.style.display = 'none';
-      menu1.style.fontWeight = 'normal';
-      menu2.style.fontWeight = 'normal';
-      menu3.style.fontWeight = 'normal';
-      menu4.style.fontWeight = 'bold';
-      menu5.style.fontWeight = 'normal';
-    } else if (menuId === 'reviews') {
-      menu.style.display = 'none';
-      glimpse.style.display = 'none';
-      crew.style.display = 'none';
-      music.style.display = 'none';
-      crew.style.display = 'none';
-      music.style.display = 'none';
-      reviews.style.display = 'flex';
-      menu1.style.fontWeight = 'normal';
-      menu2.style.fontWeight = 'normal';
-      menu3.style.fontWeight = 'normal';
-      menu4.style.fontWeight = 'normal';
-      menu5.style.fontWeight = 'bold';
-    }}
+  if (menuId === 'glimpse') {
+    menu.style.display = 'none';
+    glimpse.style.display = 'flex';
+    crew.style.display = 'none';
+    music.style.display = 'none';
+    reviews.style.display = 'none';
+    menu1.style.fontWeight = 'normal';
+    menu2.style.fontWeight = 'bold';
+    menu3.style.fontWeight = 'normal';
+    menu4.style.fontWeight = 'normal';
+    menu5.style.fontWeight = 'normal'}
+  else if (menuId === 'crew') {
+    menu.style.display = 'none';
+    glimpse.style.display = 'none';
+    crew.style.display = 'block';
+    music.style.display = 'none';
+    reviews.style.display = 'none';
+    menu1.style.fontWeight = 'normal';
+    menu2.style.fontWeight = 'normal';
+    menu3.style.fontWeight = 'bold';
+    menu4.style.fontWeight = 'normal';
+    menu5.style.fontWeight = 'normal';
+  } else if (menuId === 'music') {
+    menu.style.display = 'none';
+    glimpse.style.display = 'none';
+    crew.style.display = 'none';
+    music.style.display = 'block';
+    reviews.style.display = 'none';
+    menu1.style.fontWeight = 'normal';
+    menu2.style.fontWeight = 'normal';
+    menu3.style.fontWeight = 'normal';
+    menu4.style.fontWeight = 'bold';
+    menu5.style.fontWeight = 'normal';
+  } else if (menuId === 'reviews') {
+    menu.style.display = 'none';
+    glimpse.style.display = 'none';
+    crew.style.display = 'none';
+    music.style.display = 'none';
+    crew.style.display = 'none';
+    music.style.display = 'none';
+    reviews.style.display = 'flex';
+    menu1.style.fontWeight = 'normal';
+    menu2.style.fontWeight = 'normal';
+    menu3.style.fontWeight = 'normal';
+    menu4.style.fontWeight = 'normal';
+    menu5.style.fontWeight = 'bold';
+  }
+}
