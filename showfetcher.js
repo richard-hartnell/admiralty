b76063e7566a70769beb71e502e90d011a835ac3
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-08-11 21:24:06 -0700

    Created showfetcher.js with placeholder vars

---
+++ showfetcher.js
@@ -0,0 +1,21 @@
+let sampleImage = '/images/music/librarians.webp';
+let sampleName = 'The Librarians';
+let sampleDate = 'Tuesday, September 10';
+let sampleTime = '7p - 9p';
+let sampleDescription = 'Surreal & engaging jazzy post-neo psychedelic dream folk soundscapes.';
+let sampleAltText = 'A foursome of experienced-looking musicians standing, shoulder-to-shoulder.';
+
+const template = `
+<figure class="show">
+    <img class="show-pic" src="${sampleImage}" loading="lazy" alt="${sampleAltText}" />
+    <figcaption class="show-info">
+        <section class="music-bio">
+            <h2>${sampleName}</h2>
+            <h6>${sampleDescription}</h6>
+        </section>
+        <section class="showtime">
+            <h6>${sampleDate}<br>${sampleTime}</h6>
+        </section>
+    </figcaption>
+</figure>
+`
